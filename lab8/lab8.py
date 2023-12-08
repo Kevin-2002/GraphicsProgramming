5c278ab932a330893d4c47509e5ac517b4d4394e
G00401808@atu.ie <G00401808@atu.ie>
2023-12-08 10:35:00 +0000

    L8 image is loaded in and now subplot in progress

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -3,5 +3,20 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# print for debugging
-print('8')
+# read in image
+imgOrig = cv2.imread('./ATU.jpg')
+
+# read in a grayscale version
+imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_RGB2GRAY)
+
+# use subplot to put multiple images in a single window 
+plt.subplot(nrows, ncols,1),plt.imshow(imgOrig, cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
+plt.title(‘GrayScale’), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# display image
+cv2.imshow('Grayscale Image', imgGray)
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
