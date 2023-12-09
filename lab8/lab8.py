348d6e3c83433b82c1ae3e74cc11a79816f87110
G00401808@atu.ie <G00401808@atu.ie>
2023-12-09 16:16:44 +0000

    L8 gaussianBlur added

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -10,8 +10,22 @@ imgOrig = cv2.imread('./ATU.jpg')
 imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_RGB2GRAY)
 
 # variables for subplotting
-nrows = 1
+nrows = 3
 ncols = 2
+
+# blur variables
+KernelSizeWidth = 3
+KernelSizeHeight = 3
+
+# store 3x3 blurred image 
+imgBlur3x3 = cv2.GaussianBlur(imgGray,(KernelSizeWidth, KernelSizeHeight),0)
+
+# change the blur variables to apply a 13x13 blur
+KernelSizeWidth = 13
+KernelSizeHeight = 13
+
+# store 13x13 blurred image
+imgBlur13x13 = cv2.GaussianBlur(imgGray,(KernelSizeWidth, KernelSizeHeight),0)
 
 # use subplot to put multiple images in a single window 
 plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(imgOrig,
@@ -19,6 +33,11 @@ cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,2),plt.imshow(imgGray, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,3),plt.imshow(imgBlur3x3, cmap = 'gray')
+plt.title('3x3 Blur'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,4),plt.imshow(imgBlur13x13, cmap = 'gray')
+plt.title('13x13 Blur'), plt.xticks([]), plt.yticks([])
+
 plt.show()
 
 # display image
