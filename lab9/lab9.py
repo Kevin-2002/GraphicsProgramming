4a73bcaf3e0359eaed047f7a9de338b1622951ab
G00401808@atu.ie <G00401808@atu.ie>
2023-12-14 20:16:30 +0000

    L9 Harris corner detection added

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -2,6 +2,7 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import copy
 
 # read in images
 ATU1 = cv2.imread('./ATU1.jpg')
@@ -12,6 +13,26 @@ ATU2 = cv2.imread('./ATU2.jpg')
 # read in a grayscale version
 ATUGray1 = cv2.cvtColor(ATU1, cv2.COLOR_RGB2GRAY)
 ATUGray2 = cv2.cvtColor(ATU2, cv2.COLOR_RGB2GRAY)
+
+# Harris corner detect
+blockSize = 2
+aperture_size = 3
+k = 0.04
+
+dst = cv2.cornerHarris(ATUGray1, blockSize, aperture_size, k)
+
+#deep copy
+imgHarris = copy.deepcopy(ATUGray1)
+
+#set var
+threshold = 0.6; #number between 0 and 1
+R = 0
+B = 0
+G = 255
+for i in range(len(dst)):
+    for j in range(len(dst[i])):
+        if dst[i][j] > (threshold*dst.max()):
+            cv2.circle(imgHarris,(j,i),3,(B, G, R),-1)
 
 
 # plot images
@@ -24,11 +45,13 @@ cv2.COLOR_BGR2RGB), cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 plt.subplot(nrows, ncols,2),plt.imshow(ATUGray1, cmap = 'gray')
 plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,3),plt.imshow(dst, cmap = 'gray')
+plt.title('Harris'), plt.xticks([]), plt.yticks([])
 
-#show images
+# show images
 plt.show()
 
-#advanced 1.
+# advanced 1.
 # Find contours
 # contours, _ = cv2.findContours(MyImgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
