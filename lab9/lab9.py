1d2da2235b1f38a029293c45468189507c53fab4
G00401808@atu.ie <G00401808@atu.ie>
2023-12-14 19:13:47 +0000

    L9 ATU1Gray printed

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -3,6 +3,31 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+# read in images
+ATU1 = cv2.imread('./ATU1.jpg')
+ATU2 = cv2.imread('./ATU2.jpg')
+
+# make changes to images 
+
+# read in a grayscale version
+ATUGray1 = cv2.cvtColor(ATU1, cv2.COLOR_RGB2GRAY)
+ATUGray2 = cv2.cvtColor(ATU2, cv2.COLOR_RGB2GRAY)
+
+
+# plot images
+# variables for subplotting
+nrows = 3
+ncols = 3
+
+plt.subplot(nrows, ncols,1),plt.imshow(cv2.cvtColor(ATU1,
+cv2.COLOR_BGR2RGB), cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(nrows, ncols,2),plt.imshow(ATUGray1, cmap = 'gray')
+plt.title('GrayScale'), plt.xticks([]), plt.yticks([])
+
+#show images
+plt.show()
+
 #advanced 1.
 # Find contours
 # contours, _ = cv2.findContours(MyImgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
