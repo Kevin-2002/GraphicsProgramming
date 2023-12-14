930d6d87afc3a1e136dc5c865e32cecf935b5b17
G00401808@atu.ie <G00401808@atu.ie>
2023-12-14 18:52:03 +0000

    imported cv2, numpy and matplotlib successfully

--- lab9/lab9.py
+++ lab9/lab9.py
@@ -1,23 +1,28 @@
+# import OpenCV, numpy & matplotlib
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
 #advanced 1.
 # Find contours
-contours, _ = cv2.findContours(MyImgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# contours, _ = cv2.findContours(MyImgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Draw contours on a blank canvas
-contour_img = np.zeros_like(myImg)
+# contour_img = np.zeros_like(myImg)
 
 # Draw contours on the blank canvas
-cv2.drawContours(contour_img, contours, -1, (255, 255, 255), thickness=1)
+# cv2.drawContours(contour_img, contours, -1, (255, 255, 255), thickness=1)
 
 # Display the original image and the contours
-plt.subplot(1, 2, 1)
-plt.imshow(cv2.cvtColor(myImg, cv2.COLOR_BGR2RGB))
-plt.title('Original Image')
-plt.axis('off')
+# plt.subplot(1, 2, 1)
+# plt.imshow(cv2.cvtColor(myImg, cv2.COLOR_BGR2RGB))
+# plt.title('Original Image')
+# plt.axis('off')
 
-plt.subplot(1, 2, 2)
-plt.imshow(contour_img, cmap='gray')
-plt.title('Contours')
-plt.axis('off')
+# plt.subplot(1, 2, 2)
+# plt.imshow(contour_img, cmap='gray')
+# plt.title('Contours')
+# plt.axis('off')
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()
